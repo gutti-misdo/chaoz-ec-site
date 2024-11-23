b574a41ddf462165d0a3762bd6695a2e9e9f9512
gutti-misdo <sakahaya3@icloud.com>
2024-11-24 01:53:44 +0900

    feat: Display product listing

--- home-page/home-page.php
+++ home-page/home-page.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ホーム画面</title>
     <link rel="stylesheet" href="./css/home-page.css">
+    <link rel="stylesheet" href="./css/product-card.css">
 </head>
 <body>
     <div class="container">
@@ -15,6 +16,25 @@
             <button class="login-btn">ログイン</button>
             <button class="cart-btn">🛒</button>
         </div>
+    </div>
+    <div class="product-list">
+    <?php
+    $pdo = new PDO('mysql:host=mysql311.phy.lolipop.lan;dbname=LAA1553900-chaoz;charset=utf8', 'LAA1553900', 'Pass1105');
+
+    foreach ($pdo->query('select * from product') as $row) {
+        echo '<div class="l-wrapper">';
+        echo '<article class="card">';
+        echo '<figure class="card__thumbnail">';
+        echo '<img src="',$row['photograph'],'" class="card__image">';
+        echo '</figure>';
+        echo '<h3 class="card__title">',$row['product_name'],'</h3>';
+        echo '<p class="card__text">',$row['explanation'],'</p>';
+        echo '<p class="card__text -number">¥',number_format($row['price']),'</p>';
+        echo '</article>';
+        echo '</div>';
+    }
+
+    ?>
     </div>
 </body>
 </html>

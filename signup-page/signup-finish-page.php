85ffacf032852b2d2cf0728a3064509c85098b0c
1117wata <2401187@s.asojuku.ac.jp>
2024-11-25 11:46:10 +0900

    新規登録完了画面のcssの作成

--- signup-page/signup-finish-page.php
+++ signup-page/signup-finish-page.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="jp">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規登録完了画面</title>
+    <link rel="stylesheet" href="./css/signup-finish-page.css">
 </head>
+
 <body>
-    <h3>登録が完了しました</h3>
-    ログイン画面からログインを行ってください
     <?php
-    
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -20,15 +20,20 @@
     try {
         $pdo = new PDO('mysql:host=mysql311.phy.lolipop.lan;dbname=LAA1553900-chaoz;charset=utf8', 'LAA1553900', 'Pass1105');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql=$pdo->prepare('INSERT INTO customer(custormer_name, address, email, password, buliding_name, room_number, post_code)VALUES(?,?,?,?,?,?,?)');
-        $sql->execute([$name,$address,$email,password_hash($password, PASSWORD_DEFAULT),$BN,$RN,$yubin]);
+        $sql = $pdo->prepare('INSERT INTO customer(custormer_name, address, email, password, buliding_name, room_number, post_code)VALUES(?,?,?,?,?,?,?)');
+        $sql->execute([$name, $address, $email, password_hash($password, PASSWORD_DEFAULT), $BN, $RN, $yubin]);
     } catch (PDOException $e) {
         echo '接続失敗: ' . $e->getMessage();
     }
-        $pdo = null;
+    $pdo = null;
     ?>
-    <form action="./../login-page/login.php">
-        <button type="submit">ログイン画面へ</button>
-    </form>
+    <div class="container">
+        <h1>登録が完了しました</h1>
+        <div class="input-field">ログイン画面からログインを行ってください</div>
+        <form action="./../login-page/login.php">
+            <button class="login-button" type="submit">ログイン画面へ</button>
+        </form>
+    </div>
 </body>
+
 </html>

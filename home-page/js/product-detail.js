f2e573f1acb8b18160d0bb8f811341c55fecafd1
1117wata <2401187@s.asojuku.ac.jp>
2024-11-27 12:40:53 +0900

    create/product-detail.js

---
+++ home-page/js/product-detail.js
@@ -0,0 +1,20 @@
+/*拡大鏡のjs*/ 
+
+document.addEventListener('mousemove', function(e) {
+    var magnifier = document.querySelector('.magnifier');
+    var img = document.querySelector('.image-container img');
+    var rect = img.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    if (x > 0 && y > 0 && x < rect.width && y < rect.height) {
+        magnifier.style.display = 'block';
+        magnifier.style.left = e.clientX - magnifier.offsetWidth / 2 + 'px';
+        magnifier.style.top = e.clientY - magnifier.offsetHeight / 2 + 'px';
+        magnifier.style.backgroundImage = 'url(' + img.src + ')';
+        magnifier.style.backgroundRepeat = 'no-repeat';
+        magnifier.style.backgroundSize = img.width * 2 + 'px ' + img.height * 2 + 'px';
+        magnifier.style.backgroundPosition = '-' + x * 2 + 'px -' + y * 2 + 'px';
+    } else {
+        magnifier.style.display = 'none';
+    }
+});
--- home-page/product-detail.php
+++ home-page/product-detail.php
@@ -9,7 +9,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品詳細画面</title>
     <link rel="stylesheet" href="./css/home-page.css">
-    <!--<link rel="stylesheet" href="./css/product-card.css">!-->
     <link rel="stylesheet" href="./css/product-detail.css">
 </head>
 
@@ -79,27 +78,9 @@ session_start();
         }
 
         ?>
-    <!--これらは拡大鏡のjs!-->
-        <script>
-            document.addEventListener('mousemove', function(e) {
-                var magnifier = document.querySelector('.magnifier');
-                var img = document.querySelector('.image-container img');
-                var rect = img.getBoundingClientRect();
-                var x = e.clientX - rect.left;
-                var y = e.clientY - rect.top;
-                if (x > 0 && y > 0 && x < rect.width && y < rect.height) {
-                    magnifier.style.display = 'block';
-                    magnifier.style.left = e.clientX - magnifier.offsetWidth / 2 + 'px';
-                    magnifier.style.top = e.clientY - magnifier.offsetHeight / 2 + 'px';
-                    magnifier.style.backgroundImage = 'url(' + img.src + ')';
-                    magnifier.style.backgroundRepeat = 'no-repeat';
-                    magnifier.style.backgroundSize = img.width * 2 + 'px ' + img.height * 2 + 'px';
-                    magnifier.style.backgroundPosition = '-' + x * 2 + 'px -' + y * 2 + 'px';
-                } else {
-                    magnifier.style.display = 'none';
-                }
-            });
-        </script>
+    <!--拡大鏡js!-->
+    <script src="./js/product-detail.js"></script>
+
     </div>
 </body>
 
